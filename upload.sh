9a2751999ab1c77b423bdd4a4dc6b910d50ebf99
zhangke <398757724@qq.com>
2019-08-29 03:24:46 +0000

    ""

--- upload.sh
+++ upload.sh
@@ -2,7 +2,7 @@
 if [ ! -n "$1" ] ;then
     echo "请输入此次提交git的注释"
 else
-    git config --global user.email "398757724@qq.com"
+    git config --global user.email "zhangkefengxiu@gmail.com"
     git config  --global user.name "zhangke"
     hexo clean
     git add --all
